17af5882b4c7e6fddfe72d58a08cdb7b6d47a92a
GABRIEL ANTONIO CÁCERES SINCLAIR <gabriela.cacsin@educa.jcyl.es>
2025-11-17 16:06:17 +0100

    añadi ejemplo para ver si funciona el git y github en clase

---
+++ practicas_personales/ejemplo.java
@@ -0,0 +1,10 @@
+package practicas_personales;
+public class ejemplo {
+    public static void main(String[] args) {
+        hola();
+    }
+
+    static void hola(){
+        System.out.println("Hola mundo");
+    }
+}
