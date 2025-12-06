09f5128b26111b88806b51e98e0a544657212ac2
GabrielCacSin <gabriel.cac.sin@gmail.com>
2025-12-06 17:57:24 +0100

    pagina de estudio con todos los enunciados

--- practicas_personales/examen_tema_2_y_3_programacion/preparacion_2.java
+++ practicas_personales/examen_tema_2_y_3_programacion/preparacion_2.java
@@ -26,7 +26,7 @@ public class preparacion_2 {
 //*---------------------------------------------------------------------------------------------------------------------------------------------------
 
 /*
-*--EJERCICIO 1 PRACTICA 2.1--
+*--EJERCICIO 1 PRACTICA 2.3--
 Crear un programa en JAVA que dibuje un rectángulo formado por asteriscos(*).
 Se deben pedir base y altura por teclado. Además, añade código adicional para que dibuje dicho rectángulo sin relleno.
 */
@@ -77,7 +77,7 @@ Se deben pedir base y altura por teclado. Además, añade código adicional para
 //*---------------------------------------------------------------------------------------------------------------------------------------------------
 
 /*
-*--EJERCICIO 2 PRACTICA 2.1--
+*--EJERCICIO 2 PRACTICA 2.3--
 Crea un programa en JAVA que dibuje un triángulo rectángulo.
 Para ello solicita el símbolo con el que has dibujar el triángulo.
 Además, se debe pedir la altura del triángulo por teclado.
@@ -106,7 +106,7 @@ Además, se debe pedir la altura del triángulo por teclado.
 //*---------------------------------------------------------------------------------------------------------------------------------------------------
 
 /*
-*--EJERCICIO 3 PRACTICA 2.1--
+*--EJERCICIO 3 PRACTICA 2.3--
 Crea un programa en JAVA que dibuje un triángulo equilátero formado por sumas(+).
 Se debe pedir la altura del triángulo por teclado.
 Además, añade código adicional para que dibuje dicho rectángulo sin relleno.
@@ -175,6 +175,166 @@ Además, añade código adicional para que dibuje dicho rectángulo sin relleno.
             System.out.println("+"); //* segundo borde
             }
         }
-    }   
+    }
+//*---------------------------------------------------------------------------------------------------------------------------------------------------    
 
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+*--EJERCICIO 1 PRACTICA 2.1--
+Solicitamos un número entre el 0 y el 9999. La salida del programa debe mostrar el número con las cifras al revés.
+*/
+
+//*---------------------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+*--EJERCICIO 2 PRACTICA 2.1--
+Leer una cantidad ‘N’ y luego introducir ‘N’ números enteros. Se pide imprimir la media 
+aritmética de los números introducidos, y la diferencia del número máximo y el número 
+mínimo (resta entre el número máximo y el número mínimo).
+*/
+
+//*---------------------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+*--EJERCICIO 3 PRACTICA 2.1--
+Escribir un programa que pida números enteros hasta que se introduzca un valor menor 
+o igual a cero. A continuación, debe mostrar la multiplicación total de dichos números. 
+Se deben incluir todos los números que hemos ido introduciendo por el teclado. No se 
+pueden utilizar Arrays. 
+Ejemplo: Entrada: 3+4+5+6+8; Salida: 2880 (3*4*5*6*8)
+*/
+
+//*---------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+*--EJERCICIO 1 PRACTICA 2.2--
+Crea un programa en Java que reciba 7 palabras y luego nos muestre la palabra con 
+mayor longitud y la de menor longitud.
+*/
+
+//*---------------------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+*--EJERCICIO 2 PRACTICA 2.2--
+Crea un programa en Java que lea por teclado una cadena de texto e indique la 
+cantidad de palabras que tiene. De la clase String, únicamente se pueden utilizar los 
+métodos charAt(), trim() y length()
+*/
+
+//*---------------------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+*--EJERCICIO 3 PRACTICA 2.2--
+Definir una string a con la cadena «Esto es una string»; definir la string b con la 
+cadena «Y esto también».
+◦ Comparar las dos strings y decir si son iguales.
+◦ Crear la string «c» con la concatenación de a y b.
+◦ Crear la string «d» con la concatenacion de a y b otra vez.
+◦ Comparar las string c y d y decir si son iguales.
+◦ Indicar cuantos caracteres tiene cada una de las strings.
+◦ Copiar la string a en la string b.
+◦ Utilizar replace, para modificar la string a.
+◦ Presentar en consola el contenido de la string a y de la string b
+*/
+
+//*---------------------------------------------------------------------------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+*--EJERCICIO 1 PRACTICA 3.1--
+Implementa un método que, dados dos números enteros, los divida devolviendo un 
+float
+*/
+
+//*---------------------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+*--EJERCICIO 2 PRACTICA 3.1--
+Impementa un método que dado un número, diga si este es impar.
+*/
+
+//*---------------------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+*--EJERCICIO 3 PRACTICA 3.1--
+Implementa un método para mostrar un menú de N opciones, con su número 
+correspondiente, la última opción será para salir.
+*/
+
+//*---------------------------------------------------------------------------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+*--EJERCICIO 1 PRACTICA 3.2--
+Implementa un método que a partir del lado, dibuje en cuadrado formado por 
+asteriscos. Crea un método adicional para dibujar dicho cuadrado sin relleno.
+*/
+
+//*---------------------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+*--EJERCICIO 2 PRACTICA 3.2--
+Implementa un método que a partir de la altura, dibuje un triángulo rectángulo formado 
+por asteriscos. Crea un método adicional para dibujar dicho triángulo sin relleno.
+*/
+
+//*---------------------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+*--EJERCICIO 3 PRACTICA 3.2--
+Crear un menú con 5 opciones, una por cada tipo de figura, y una opción para salir. Se 
+deberá elegir una opción y realizar la acción correspondiente. El programa no terminará 
+hasta que seleccionemos la opción SALIR del menú principal. Cómo sugerencia, utiliza 
+figuras ya creadas. Son diferentes los cuadrados rellenos a los que están vacíos.
+*/
+
+//*---------------------------------------------------------------------------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+*--EJERCICIO 1 PRACTICA 3.3--
+Crea un método que obtenga el resultado de elevar un número a otro. Ambos números 
+se deben pasar como parámetros. Los números deben ser positivos. RECURSIVIDAD
+*/
+
+//*---------------------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+*--EJERCICIO 2 PRACTICA 3.3--
+Crea un método que compruebe si una palabra está inversamente ordenada 
+alfabéticamente. RECURSIVIDAD
+*/
+
+//*---------------------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+*--EJERCICIO 3 PRACTICA 3.3--
+Crea un método que obtenga la suma de los inversos de los números naturales desde 1 
+hasta N. Se debe leer como parámetro el número N, que debe ser mayor que cero. Se 
+debe imprimir toda la cadena por consola. Por ejemplo, para N=4 → ( 1/1+1/2+1/3+1/4 = 2.083333333333333).
+*/
+
+//*---------------------------------------------------------------------------------------------------------------------------------------------------
 }
